4bf39b45a63f6e2a3a854ae5c33b90135d73db79
Dehui Yang <Dehui@Dehuis-MacBook-Pro.local>
2018-06-30 16:37:33 -0600

    first commit for lab1

---
+++ lab1/HelloWorld.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
+
+/*
+1. All code in java must be part of a class
+2. We delimit the beginning and end of segments of code using { and }.
+3. All statements in java must end in a semi-colon.
+4. For a code to run, we need public static void main(String[] args).
+*/
